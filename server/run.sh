d7a3a60c999ab28a7793a10a9ad1bb8d072e8add
bridgekat <zhanrong.qiao21@imperial.ac.uk>
2023-06-09 14:55:57 +0100

    Debugging CI script

--- server/run.sh
+++ server/run.sh
@@ -10,15 +10,26 @@ mkdir -p "$DIR"
 # Kill old process if still running
 if [ -e "$DIR/gunicorn.pid" ]
 then
-  kill `cat $DIR/gunicorn.pid`
+  PID=`cat $DIR/gunicorn.pid`
+  kill $PID
+
+  # Wait until old process stops
+  # See: https://stackoverflow.com/a/29281720
+  while kill -0 $PID
+  do
+    echo "Waiting for old server process to stop..."
+    sleep 1
+  done
 fi
 
 # Wait until socket is no longer used
 while [ -e "$DIR/gunicorn.sock" ]
 do
-  echo "Waiting for socket..."
+  echo "Waiting for socket file to become available..."
   sleep 1
 done
+
+echo "Starting server..."
 
 # Start `gunicorn` in daemon mode
 export DJANGO_SETTINGS_MODULE="config.settings"
@@ -31,3 +42,5 @@ gunicorn "config.wsgi:application" \
 --log-level="info" \
 --log-file="$DIR/gunicorn.log" \
 --daemon
+
+echo "Gunicorn server started. Log file is at \"$DIR/gunicorn.log\"."
