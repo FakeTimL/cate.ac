0178e52de46b653f720f1151dc74abf911400eeb
bridgekat <zhanrong.qiao21@imperial.ac.uk>
2023-06-09 13:27:11 +0100

    Delete migration file

--- server/build.sh
+++ server/build.sh
@@ -41,7 +41,7 @@ mkdir -p "$DIR"
 # ```
 
 python3 manage.py collectstatic --noinput
-python3 manage.py migrate main 0005_question_mark_minimum
+python3 manage.py migrate main
 
 # Running/restarting server:
 #
